fc0f4437f62202e92b973fd848105829f1c380c3
200Tigersbloxed <45884377+200Tigersbloxed@users.noreply.github.com>
2021-08-24 19:09:18 -0400

    LabratEyeTracking | Multithreading
    Adds multithreading to LabratEyeTracking for better performance

--- LabratEyeTracking/LabratEyeTracking/PimaxHelper.cs
+++ LabratEyeTracking/LabratEyeTracking/PimaxHelper.cs
@@ -3,33 +3,59 @@ using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Threading;
 
 namespace LabratEyeTracking
 {
     public static class PimaxHelper
     {
+        public static bool EnableEyeTracking = false;
         public static Pimax.EyeTracking.EyeTracker eyeTracker;
 
-        public static void Initialize()
+        private static Thread PimaxWorker;
+
+        public static void Initialize() => Start();
+
+        public static void Start() 
         {
-            eyeTracker = new Pimax.EyeTracking.EyeTracker();
-            //eyeTracker.OnUpdate += OnEyeTrackerUpdate;
-            eyeTracker.OnStart += OnEyeTrackerStart;
-            eyeTracker.OnStop += OnEyeTrackerStop;
-            eyeTracker.Start();
+            PimaxWorker = new Thread(delegate ()
+            {
+                eyeTracker = new Pimax.EyeTracking.EyeTracker();
+                //eyeTracker.OnUpdate += OnEyeTrackerUpdate;
+                eyeTracker.OnStart += OnEyeTrackerStart;
+                eyeTracker.OnStop += OnEyeTrackerStop;
+                EnableEyeTracking = true;
+                eyeTracker.Start();
+                // Keep the Thread Alive
+                while (EnableEyeTracking) { Thread.Sleep(10); }
+            });
+        }
+        public static void Kill() 
+        {
+            if (!EnableEyeTracking)
+            {
+                LogHelper.Warn("Eye Tracking is not enabled, can't kill.");
+                return;
+            }
+            // Abort Thread
+            if (PimaxWorker != null)
+            {
+                if (PimaxWorker.IsAlive)
+                    PimaxWorker.Abort();
+            }
+            PimaxWorker = null;
+            // Stop eye tracking and reset
+            eyeTracker.Stop();
+            eyeTracker = null;
         }
 
-        public static void Start() { eyeTracker.Start(); }
-        public static void Kill() { eyeTracker.Stop(); }
-
-        private static void OnEyeTrackerStart()
-        {
-            LogHelper.Debug("Pimax Eye Tracker Started!");
-        }
+        private static void OnEyeTrackerStart() => LogHelper.Debug("Pimax Eye Tracker Started!");
 
         private static void OnEyeTrackerStop()
         {
             LogHelper.Debug("Pimax Eye Tracker Stopped!");
+            // Call Kill in-case we weren't supposed to stop
+            Kill();
         }
     }
 }
--- LabratEyeTracking/LabratEyeTracking/SRanipalHelper.cs
+++ LabratEyeTracking/LabratEyeTracking/SRanipalHelper.cs
@@ -16,6 +16,7 @@ namespace LabratEyeTracking
     {
         public static bool EyeTrackingEnabled = false;
         private static bool VerifyEyeError(this Error error) => error != Error.WORK && error != Error.UNDEFINED && error != (Error)1051;
+        private static Thread SRanipalWorker;
 
         public static EyeData_v2 EyeData;
 
@@ -30,14 +31,39 @@ namespace LabratEyeTracking
             }
             eyeError = SRanipal_API.Initial(SRanipal_Eye_v2.ANIPAL_TYPE_EYE_V2, IntPtr.Zero);
 
-            if (eyeError.VerifyEyeError()) { LogHelper.Error("Failed to Initialize SRanipal Eye Tracking! EXCEPTION: " + eyeError.ToString()); }
-            else { LogHelper.Debug("SRanipal Eye Tracking Initialized!"); EyeTrackingEnabled = true;  }
+            if (eyeError.VerifyEyeError()) { LogHelper.Error("Failed to Initialize SRanipal Eye Tracking! EXCEPTION: " + eyeError.ToString()); return; }
+            LogHelper.Debug("SRanipal Eye Tracking Initialized!"); EyeTrackingEnabled = true;
+            StartThread();
+        }
+
+        public static void StartThread()
+        {
+            SRanipalWorker = new Thread(delegate()
+            {
+                while (EyeTrackingEnabled)
+                {
+                    UpdateEyeData();
+                    Thread.Sleep(10);
+                }
+            });
         }
 
         public static void Kill()
         {
-            if (EyeTrackingEnabled) { SRanipal_API.Release(SRanipal_Eye_v2.ANIPAL_TYPE_EYE_V2); LogHelper.Debug("Killed SRanipal"); }
-            else { LogHelper.Warn("Eye Tracking is not enabled, can't kill."); }
+            if (!EyeTrackingEnabled)
+            {
+                LogHelper.Warn("Eye Tracking is not enabled, can't kill.");
+                return;
+            }
+            // Abort Thread
+            if (SRanipalWorker != null)
+            {
+                if (SRanipalWorker.IsAlive)
+                    SRanipalWorker.Abort();
+            }
+            SRanipalWorker = null;
+            // Release Eye Tracking
+            SRanipal_API.Release(SRanipal_Eye_v2.ANIPAL_TYPE_EYE_V2); LogHelper.Debug("Killed SRanipal");
         }
 
         public static void UpdateEyeData() { if (EyeTrackingEnabled) { SRanipal_Eye_API.GetEyeData_v2(ref EyeData); } }
